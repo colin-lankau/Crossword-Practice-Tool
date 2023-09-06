ffba0a7875f66213b7857a856d092ec9df719194
colin-lankau <110795769+colin-lankau@users.noreply.github.com>
2023-09-05 22:04:52 -0400

    09-05-23 Update

--- Board.py
+++ Board.py
@@ -2,25 +2,24 @@ class Board:
 
 
     def __init__(self, a_clues: list, a_answers: list, d_clues: list, d_answers: list, isSunday: bool):
-        self.a_clues = a_clues
-        self.a_answers = a_answers
-        self.d_clues = d_clues
-        self.d_answers = d_answers
+        self.a_clues = list(a_clues)
+        self.a_answers = list(a_answers)
+        self.d_clues = list(d_clues)
+        self.d_answers = list(d_answers)
         self.isSunday = isSunday
         self.length = 15 if not isSunday else 21
         self.board = [ [] for i in range(self.length) ]
 
 
-    def __post_init__(self):
-        self.build_Board()
-
-
     def __repr__(self):
-        return_text = ""
+        return_text = "| "
         for row in self.board:
             for char in row:
-                return_text += char
+                return_text += f"{char} | "
+            return_text.rstrip(" | ")
             return_text += "\n"
+            return_text += '-' * 65
+            return_text += "\n| "
         return return_text
 
 
@@ -32,29 +31,33 @@ class Board:
 
         # generate the board through some sort of backtracking algorithm
         x, y = 0, 0
-        for ac in self.a_clues:
+        for ac in self.a_answers:
+            print(ac)
+            print()
+            print(self.board)
+            print()
             if len(ac) > self.length - x:
-
                 y += 1
                 x = 0
-            self.add_to_Board(ac, x, y)
+                self.add_to_Board(ac, y)
+            else:
+                self.add_to_Board(ac, y)
             x += len(ac)
+            self.fill_black_squares(y)
     
 
-    def add_to_Board(self, word: str, x: int, y: int):
+    def add_to_Board(self, word: str, row: int):
         '''
         Receives word in string form, breaks it into chars, and adds it to board
         '''
 
         for char in word:
-            self.board[x][y] = char
-            x += 1
+            self.board[row].append(char)
 
     
-    def fill_black_squares(self, x: int, y: int):
+    def fill_black_squares(self, row: int):
         '''
         Fills in black squares if necessary to the end of the grid
         '''
 
-        for i in range(x, self.length, 1):
-            self.board[x][y] = "0"
+        self.board[row].append("~")
--- Scrape.py
+++ Scrape.py
@@ -1,45 +1,40 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
-from datetime import date, datetime
-import calendar
 # from selenium.webdriver.common.keys import Keys
 # from selenium.webdriver.support import expected_conditions as EC
 # from selenium.webdriver.support.ui import WebDriverWait
-from Board import Board
 
-# setup
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--mute-audio")
-driver = webdriver.Chrome(options=chrome_options)
-year, month, day = str(date.today()).split("-")
-today = f"{month}-{day}-{year[-2:]}"
-print(today)
-driver.get(f"https://nytcrosswordanswers.org/nyt-crossword-answers-{today}/")
-print(driver.title)
-time.sleep(3)
+def Scrape(today):
 
-uls = driver.find_elements(By.TAG_NAME, "ul")
-across = uls[1].find_elements(By.TAG_NAME, "li")
-down = uls[2].find_elements(By.TAG_NAME, "li")
+    # setup
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--mute-audio")
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get(f"https://nytcrosswordanswers.org/nyt-crossword-answers-{today}/")
+    print(driver.title)
+    time.sleep(3)
 
-across_clues = []
-across_answers = []
-for group in across:
-    clue = group.find_element(By.TAG_NAME, "a").get_attribute('innerText')
-    answer = group.find_element(By.TAG_NAME, "span").get_attribute('innerText')
-    across_clues.append(clue)
-    across_answers.append(answer)
+    uls = driver.find_elements(By.TAG_NAME, "ul")
+    across = uls[1].find_elements(By.TAG_NAME, "li")
+    down = uls[2].find_elements(By.TAG_NAME, "li")
 
-down_clues = []
-down_answers = []
-for group in down:
-    clue = group.find_element(By.TAG_NAME, "a").get_attribute('innerText')
-    answer = group.find_element(By.TAG_NAME, "span").get_attribute('innerText')
-    down_clues.append(clue)
-    down_answers.append(answer)
+    across_clues = []
+    across_answers = []
+    for group in across:
+        clue = group.find_element(By.TAG_NAME, "a").get_attribute('innerText')
+        answer = group.find_element(By.TAG_NAME, "span").get_attribute('innerText')
+        across_clues.append(clue)
+        across_answers.append(answer)
 
-board = Board(across_clues, across_answers, down_clues, down_answers, datetime.today().strftime('%A') == 'Sunday')
-print(board)
+    down_clues = []
+    down_answers = []
+    for group in down:
+        clue = group.find_element(By.TAG_NAME, "a").get_attribute('innerText')
+        answer = group.find_element(By.TAG_NAME, "span").get_attribute('innerText')
+        down_clues.append(clue)
+        down_answers.append(answer)
 
-driver.quit()
+    driver.quit()
+    
+    return across_clues, across_answers, down_clues, down_answers
